873c1616539617d951965577a0c85ab35b105e09
Timo Post <timo.post@student.uva.nl>
2023-12-30 17:25:25 +0100

    Enhanced gift wrapping algorithm Gift wrapping now checks for distance if 2 points are colinear with the last point.

--- gift_wrapping.py
+++ gift_wrapping.py
@@ -11,21 +11,35 @@ def left_of(a: tuple, b: tuple, c: tuple) -> bool:
     return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0]) > 0
 
 
+def right_of(a: tuple, b: tuple, c: tuple) -> bool:
+    """
+    Check if point c is right of the line from a to b.
+    """
+    return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0]) < 0
+
+
+def dist(a: tuple, b: tuple) -> float:
+    """
+    Compute the squared distance between two points.
+    """
+    return ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
+
+
 def gift_wrapping(points: list[tuple]) -> list[tuple]:
     """
     Find the convex hull of a set of points using the gift wrapping algorithm
     (aka Jarvis march).
     """
-    # Find the leftmost point. If there are several leftmost points, pick the
-    # one with the lowest y-coordinate.
     point_on_hull = min(points)
-
     hull = []
     while True:
         hull.append(point_on_hull)
         endpoint = points[0]
         for point in points[1:]:
-            if endpoint == point_on_hull or left_of(hull[-1], endpoint, point):
+            if (endpoint == point_on_hull  # Endpoint is the previous point on the hull.
+                    or left_of(hull[-1], endpoint, point)  # Point is left of endpoint.
+                    or (not right_of(hull[-1], endpoint, point)  # Point is colinear with endpoint.
+                        and dist(hull[-1], point) > dist(hull[-1], endpoint))):  # Point is further than endpoint.
                 endpoint = point
         point_on_hull = endpoint
         if endpoint == hull[0]:
